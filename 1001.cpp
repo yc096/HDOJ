8b35fdf3bb2d07847d0992c7cc29fde72f1d4b07
Chen <40769442+yc096@users.noreply.github.com>
2019-03-26 22:44:18 +0800

    1001
    更新1001，注意越界问题。而且该题是问一答一

--- 1001.cpp
+++ 1001.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
 using namespace std;
-class ListNode
-{
-	public:
-		ListNode* next;
-		int data;
-		List()
-		{
-			next = NULL;
-			data = 0;
-		}
-}; 
 int main(){
-	//根节点 
 	int n;
-
-	ListNode* Root = new ListNode;
-	Root->data = -1;
-	ListNode* p = Root;//尾指针 
-	
 	while(cin>>n)
 	{
-		ListNode* Node = new ListNode;
-		p->next = Node;
-		Node->data = n;
-		p = Node;
-	}
-	p = Root;
-//
-	while(p->next!=NULL)
-	{
-		cout<<p->data;
-		p = p->next;
+		//需要注意n*(n+1)可能会越界所以先除2 
+		if(n%2==0)
+			cout<<(n/2)*(n+1)<<endl<<endl;
+		else
+			cout<<((n+1)/2)*n<<endl<<endl;
 	}
 	return 0;
 } 
+
+
 
